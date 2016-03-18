ae14a593a9a3421c682b2cf46f521b20e7344f7f
Micaël <micael.mbagira@icloud.com>
2016-03-18 16:07:53 +0100

    Isolate hello component in spec

--- generators/hello/inject/templates/src/app/hello.spec.js
+++ generators/hello/inject/templates/src/app/hello.spec.js
@@ -1,6 +1,11 @@
 describe('hello component', function () {
-  beforeEach(angular.mock.module('app'));
-  beforeEach(angular.mock.module('<%- templateUrl %>'));
+  beforeEach(module('app', function ($provide) {
+    $provide.factory('app', function () => {
+      return {
+        templateUrl: 'app/hello.html'
+      };
+    });
+  }));
   it('should render hello world', angular.mock.inject(function ($rootScope, $compile) {
     var element = $compile('<app>Loading...</app>')($rootScope);
     $rootScope.$digest();
--- generators/hello/inject/templates/src/app/hello.spec.ts
+++ generators/hello/inject/templates/src/app/hello.spec.ts
@@ -1,7 +1,13 @@
 /// <reference path="../../typings/main.d.ts" />
 
 describe('hello component', function() {
-  beforeEach(angular.mock.module('app'));
+  beforeEach(module('app', function ($provide: ng.auto.IProvideService) {
+    $provide.factory('app', function () => {
+      return {
+        templateUrl: 'app/hello.html'
+      };
+    });
+  }));
   it('should render hello world', inject(function($rootScope: ng.IRootScopeService, $compile: ng.ICompileService) {
     const element = $compile('<app>Loading...</app>')($rootScope);
     $rootScope.$digest();
