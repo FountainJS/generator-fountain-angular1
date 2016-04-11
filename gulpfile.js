0b38227a027cf32bf3f0529b0fe739a649339995
Mehdy Dara <mdara@eleven-labs.com>
2016-04-11 17:19:32 +0200

    Update gulpfile && cleans deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
+'use strict';
+
+const path = require('path');
 const gulp = require('gulp');
-const gutil = require('gulp-util');
 const eslint = require('gulp-eslint');
 const excludeGitignore = require('gulp-exclude-gitignore');
 const mocha = require('gulp-mocha');
 const istanbul = require('gulp-istanbul');
+const nsp = require('gulp-nsp');
+const plumber = require('gulp-plumber');
 
-gulp.task('linter', eslintCheck);
-gulp.task('default', gulp.series('linter', gulp.series(istanbulCover, mochaTest)));
+gulp.task('nsp', nodeSecurityProtocol);
+gulp.task('watch', watch);
+gulp.task('static', eslintCheck);
+gulp.task('pre-test', istanbulCover);
+gulp.task('test', gulp.series('pre-test', mochaTest));
+
+gulp.task('prepublish', gulp.series('nsp'));
+gulp.task('default', gulp.series('static', 'test'));
+
+function nodeSecurityProtocol(cb) {
+  nsp({package: path.resolve('package.json')}, cb);
+}
 
 function eslintCheck() {
-  return gulp.src('**/*.js')
+  return gulp.src(['**/*.js', '!**/templates/**'])
     .pipe(excludeGitignore())
     .pipe(eslint())
     .pipe(eslint.format())
@@ -17,17 +31,24 @@ function eslintCheck() {
 }
 
 function istanbulCover() {
-  return gulp.src('generators/*/index.js')
-    .pipe(istanbul({includeUntested: true}))
+  return gulp.src(['**/*.js', '!**/templates/**'])
+    .pipe(excludeGitignore())
+    .pipe(istanbul({
+      includeUntested: true
+    }))
     .pipe(istanbul.hookRequire());
 }
 
 function mochaTest() {
   return gulp.src('test/**/*.js')
+    .pipe(plumber())
     .pipe(mocha({reporter: 'spec'}))
-    .once('error', err => {
-      gutil.log(gutil.colors.red('[Mocha]'), err.toString());
+    .once('error', () => {
       process.exit(1);
     })
     .pipe(istanbul.writeReports());
+}
+
+function watch() {
+  gulp.watch('**/*.js', ['test']);
 }
