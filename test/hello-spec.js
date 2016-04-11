cbbecd511d9eae80dfabed08b67877b9abd7ea39
Matthieu Lux <matthieu.lux@gmail.com>
2016-04-11 21:23:10 +0200

    fix hello spec test

--- test/hello-spec.js
+++ test/hello-spec.js
@@ -5,17 +5,17 @@ const TestUtils = require('fountain-generator').TestUtils;
 
 describe('generator fountain gulp partials', () => {
   beforeEach(function () {
-    this.context = TestUtils.mock();
-    require('../generators/app/index');
+    this.context = TestUtils.mock('hello/modules');
+    require('../generators/hello/modules/index');
   });
 
   it('should remove angular.mock prefix with TS and systemjs', function () {
-    const getFile = () => this.context.copyTemplate['src/app/hello.component.spec.js'];
+    const getFile = () => this.context.copyTemplate['src/app/hello.spec.js'];
     TestUtils.call(this.context, 'writing', {
       framework: 'angular1',
       css: 'scss'
     });
-    expect(getFile()).to.match(/angular\.mock\.module\('hello/);
+    expect(getFile()).to.match(/angular\.mock\.module\('fountainHello/);
     expect(getFile()).to.match(/angular\.mock\.inject\(function/);
     TestUtils.call(this.context, 'writing', {
       modules: 'systemjs',
@@ -23,9 +23,9 @@ describe('generator fountain gulp partials', () => {
       framework: 'angular1',
       css: 'scss'
     });
-    expect(getFile()).not.to.match(/angular\.mock\.module\('hello/);
+    expect(getFile()).not.to.match(/angular\.mock\.module\('fountainHello/);
     expect(getFile()).not.to.match(/angular\.mock\.inject\(function/);
-    expect(getFile()).to.match(/module\('hello/);
-    expect(getFile()).to.match(/inject\(function/);
+    expect(getFile()).to.match(/module\('fountainHello/);
+    expect(getFile()).to.match(/inject\(\(/);
   });
 });
