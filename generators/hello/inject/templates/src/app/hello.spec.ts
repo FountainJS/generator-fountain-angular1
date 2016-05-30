7177af78bb560a9ac208339cbe186b76209e7561
Micaël <micael.mbagira@icloud.com>
2016-05-30 09:41:41 +0200

    Edit typings base url since upgrade

--- generators/hello/inject/templates/src/app/hello.spec.ts
+++ generators/hello/inject/templates/src/app/hello.spec.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../typings/main.d.ts" />
+/// <reference path="../../typings/index.d.ts" />
 
 describe('hello component', () => {
   beforeEach(module('app', ($provide: ng.auto.IProvideService) => {
--- generators/techs/inject/templates/src/app/footer.spec.ts
+++ generators/techs/inject/templates/src/app/footer.spec.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../typings/main.d.ts" />
+/// <reference path="../../typings/index.d.ts" />
 
 describe('footer component', () => {
   beforeEach(module('app', ($provide: ng.auto.IProvideService) => {
