f84006a494b73c834a58bc3d19d4839270ddfe76
Micaël <micael.mbagira@icloud.com>
2016-06-10 11:11:07 +0200

    Add @ngInject annotation for ngAnnotate

--- generators/app/templates/src/routes.js
+++ generators/app/templates/src/routes.js
@@ -6,6 +6,7 @@ angular
 module.exports = routesConfig;
 <% } -%>
 
+/** @ngInject */
 function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
   $urlRouterProvider.otherwise('/');
--- generators/app/templates/src/routes.ts
+++ generators/app/templates/src/routes.ts
@@ -8,6 +8,7 @@ angular
 export default routesConfig;
 <% } -%>
 
+/** @ngInject */
 function routesConfig($stateProvider: angular.ui.IStateProvider, $urlRouterProvider: angular.ui.IUrlRouterProvider, $locationProvider: angular.ILocationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
   $urlRouterProvider.otherwise('/');
